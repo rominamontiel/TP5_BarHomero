516afeb335afe951166f732782bd2ddd4f1e8b56
rominamontiel <romiiemma@hotmail.com>
2016-07-06 03:54:15 -0300

    Sobreescribiendo equals y hashCode

--- src/ar/edu/unlam/basica2/Cliente.java
+++ src/ar/edu/unlam/basica2/Cliente.java
@@ -2,17 +2,17 @@ package ar.edu.unlam.basica2;
 
 public class Cliente implements Comparable<Cliente> {
 	
-	private int edad;
+	private Integer edad;
 	private String nombre;
 	
 	
-	public Cliente (int ed, String nom){ 
+	public Cliente (Integer ed, String nom){ 
 		this.edad=ed;
 		this.nombre=nom;
 	}
 
 	
-	public int getEdad(){
+	public Integer getEdad(){
 		return edad;
 	}
 	
@@ -35,17 +35,50 @@ public class Cliente implements Comparable<Cliente> {
 		
 	}
 		
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((edad == null) ? 0 : edad.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (edad == null) {
+			if (other.edad != null)
+				return false;
+		} else if (!edad.equals(other.edad))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}	
+	
+	
 	/*
 	@Override
 	public boolean equals (Object obj){
 		return true; // BORRAR !! 
 	}
-*/
+
 	
 	@Override
 	public int hashCode(){
 		return 1;
-	}
+	}*/
 		
 } // FIN Cliente
 
